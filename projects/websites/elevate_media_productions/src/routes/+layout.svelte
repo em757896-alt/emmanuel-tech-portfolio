d64c05a25ff892c6dc8e7bdfeb617ee465c327f0
em757896-alt <em757896@gmail.com>
2026-09-14 09:23:54 +0300

    fix: import app.css in root layout so Tailwind CSS is emitted

--- projects/websites/elevate_media_productions/src/routes/+layout.svelte
+++ projects/websites/elevate_media_productions/src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import '../app.css';
   import { onMount } from 'svelte';
   import Navbar from '$lib/components/layout/Navbar.svelte';
   import Footer from '$lib/components/layout/Footer.svelte';
